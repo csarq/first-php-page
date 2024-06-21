35cd597b0994cdd1ad83a2ffef49093d993b0df0
csarq <catherinesarquis@gmail.com>
2024-06-21 02:17:13 +0100

    rename files

--- index.php
+++ index.php
@@ -1,37 +1,54 @@
 <?php
-$name = 'Catherine';
-$name = 'José';
-$isDev = true;
-$faveAnimal = 'rabbit';
-$age = 2;
 
-$isOld = $age > 28;
+const API_URL = 'https://whenisthenextmcufilm.com/api';
+# initialise a new curl session; ch = curl handle
+$ch = curl_init(API_URL);
 
-define('logo_url', 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
+// Indicate that we want to get the request response and not display it on the screen
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+/* execute the request 
+and store the response 
+*/
+
+$result = curl_exec($ch);
+
+//an alternative way is to use file_get_contents
+// $result = file_get_contents(API_URL);
+
+$data = json_decode($result, true);
+curl_close($ch);
 
 
-$output = "Hola $name, con una $faveAnimal ";
-
-$outputAge = match (true) {
-$age < 3 => "Eres un bebé",
-$age < 10 => "Eres un niño",
-};
-
-$bestLanguages = ["PHP", "JavaScript", "Python", "Java", "C#"];
 
 ?>
 
-<ul>
-  <?php foreach ($bestLanguages as $language) : ?> 
-    <li><?= $language ?></li>
-    <?php endforeach; ?>
-</ul>
-<h3>El mejor lenguaje es <?= $bestLanguages[3] ?></h3>
+<head>
+  <title>The next Marvel film</title>
+  <meta name = "description" content="The next Marvel film" />
+  <meta name = "viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Centered viewport --> 
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+/>
 
-<h1>
-<?= $outputAge ?>
+</head>
 
-</h1>
+<main>
+  <pre style="font-size: 10px; overflow: scroll;"><?= var_dump($data); ?></pre>
+    
+  <section>
+    <img src="<?= $data["poster_url"]; ?> "width="200" alt="Poster of <?=$data["title"]; ?>"
+    style="border-radius: 16px;" />
+    
+  </section>
+
+  <hgroup>
+    <h3><?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?></h3>
+    <p>Fecha de estreno: <?=$data["release_date"];?> </p>
+    <p>La siguiente es: <?= $data["following_production"] ["title"]; ?> </p>
+  </hgroup>
+</main>
 
 <style>
   :root {
